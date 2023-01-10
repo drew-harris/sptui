4b0d208719aa6b9f4150ca8c3a71010f5fbedc00
Drew Harris <harrisd@smu.edu>
2023-01-10 01:34:02 -0600

    Add hint paragraph

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,18 +1,28 @@
 use tui::{
     backend::Backend,
-    widgets::{Block, Borders},
+    layout::Margin,
+    widgets::{Block, Borders, Paragraph},
     Frame,
 };
 
 use crate::app::App;
 
-pub fn draw_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+pub fn draw_ui<B: Backend>(f: &mut Frame<B>, app: &App) {
     let size = f.size();
+
     let title = match app.is_clicked {
         true => "Clicked".to_string(),
         false => "Not Clicked".to_string(),
     };
 
     let block = Block::default().title(title).borders(Borders::ALL);
+    let hint = Paragraph::new("Press \"J\" to toggle");
+    f.render_widget(
+        hint,
+        size.inner(&Margin {
+            vertical: 2,
+            horizontal: 4,
+        }),
+    );
     f.render_widget(block, size);
 }
