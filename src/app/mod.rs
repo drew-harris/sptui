598217fcf12c8a022d606beb35bbb7f5ae1762b7
Drew Harris <harrisd@smu.edu>
2023-06-12 10:48:19 -0500

    Remove is clicked

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,14 +1,12 @@
 use rspotify::model::FullTrack;
 
 pub struct App {
-    pub is_clicked: bool,
     pub currently_playing: Option<FullTrack>,
 }
 
 impl App {
     pub fn new() -> Self {
         App {
-            is_clicked: false,
             currently_playing: None,
         }
     }
